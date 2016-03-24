0c9f7c0aa4834fbc2332bb505b094b6d6b1c4a63
Jon <lasercar@users.noreply.github.com>
2016-03-23 23:21:29 -0500

    basic chat listener

--- src/main/java/io/github/lasercar/simplemention/SimpleMention.java
+++ src/main/java/io/github/lasercar/simplemention/SimpleMention.java
@@ -1,12 +1,32 @@
 package io.github.lasercar.simplemention;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpleMention extends JavaPlugin implements Listener {
 
+    //init
+
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    //events
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+
+        if (message.contains("@")) {
+            //event.setMessage();
+            getLogger().info("mention found");
+        } else {
+            getLogger().info("mention not found");
+        }
     }
 
 }
