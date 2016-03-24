e4824b97cb3923c492290f8992a1814a1edc0ffb
Jon <lasercar@users.noreply.github.com>
2016-03-23 22:11:10 -0500

    boilerplate 3

--- src/main/java/io/github/lasercar/simplemention/SimpleMention.java
+++ src/main/java/io/github/lasercar/simplemention/SimpleMention.java
@@ -1,5 +1,12 @@
 package io.github.lasercar.simplemention;
 
-public class SimpleMention {
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleMention extends JavaPlugin implements Listener {
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
 
 }
