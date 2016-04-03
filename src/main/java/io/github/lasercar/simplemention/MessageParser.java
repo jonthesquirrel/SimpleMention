a077cf814f2d4001c635eaf890cf39909e80bcfd
Jon <lasercar@users.noreply.github.com>
2016-04-03 15:34:37 -0500

    Get config values (verbosely) - Closes #18
    Had to put "static" in lots of places for it to work

--- src/main/java/io/github/lasercar/simplemention/MessageParser.java
+++ src/main/java/io/github/lasercar/simplemention/MessageParser.java
@@ -4,25 +4,13 @@ import org.bukkit.ChatColor;
 
 public class MessageParser {
 
-    //TODO: config class?
-//    String formatHandle = ChatColor.translateAlternateColorCodes('&', config.getString("format-handle"));
-//    String formatMention = ChatColor.translateAlternateColorCodes('&', config.getString("format-mention"));
-//    String formatReset = ChatColor.translateAlternateColorCodes('&', config.getString("format-reset"));
-    //TEMP: hardcode
-    static String formatHandle = ChatColor.translateAlternateColorCodes('&', "&e");
-    static String formatMention = ChatColor.translateAlternateColorCodes('&', "&l");
-    static String formatReset = ChatColor.translateAlternateColorCodes('&', "&7");
+    static String formatHandle = ChatColor.translateAlternateColorCodes('&', SimpleMention.config.getString("format-handle"));
+    static String formatMention = ChatColor.translateAlternateColorCodes('&', SimpleMention.config.getString("format-mention"));
+    static String formatReset = ChatColor.translateAlternateColorCodes('&', SimpleMention.config.getString("format-reset"));
 
     public static boolean hasMentions(String message) {
         return message.contains("@");
     }
-
-//    public Collection findMentions(String message) {
-//        Pattern pattern = Pattern.compile("@(\\w+)");
-//        Matcher matcher = pattern.matcher(message);
-//
-//        return
-//    }
 
     public static String highlightMention(String mention) {
         return formatHandle + "@" + formatMention + mention + formatReset;
--- src/main/java/io/github/lasercar/simplemention/SimpleMention.java
+++ src/main/java/io/github/lasercar/simplemention/SimpleMention.java
@@ -1,12 +1,17 @@
 package io.github.lasercar.simplemention;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpleMention extends JavaPlugin {
 
+    public static FileConfiguration config;
+
     public void onEnable() {
+        config = getConfig();
+
         saveDefaultConfig();
-        getConfig().options().copyDefaults(true);
+        config.options().copyDefaults(true);
         saveConfig();
 
         getServer().getPluginManager().registerEvents(new Listeners(), this);
